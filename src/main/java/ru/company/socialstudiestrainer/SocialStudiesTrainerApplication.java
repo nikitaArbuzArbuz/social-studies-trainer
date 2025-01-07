4dad30e69a68fe55482ae2cab2409b76cd29406d
nikitaArbuzArbuz <talker_64@vk.com>
2025-01-07 13:36:22 +0300

    Initial commit

---
+++ src/main/java/ru/company/socialstudiestrainer/SocialStudiesTrainerApplication.java
@@ -0,0 +1,13 @@
+package ru.company.socialstudiestrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialStudiesTrainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialStudiesTrainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/company/socialstudiestrainer/SocialStudiesTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.company.socialstudiestrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialStudiesTrainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
